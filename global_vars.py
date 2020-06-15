4c3d2e8b835374a51600c6a138ca481ddea7b074
javadebadi <javad.ebadi.1990@gmail.com>
2020-06-15 19:21:19 +0430

    PEP8 style: arranged global_vars.py to be accroding to PEP8 styles

--- global_vars.py
+++ global_vars.py
@@ -1,10 +1,21 @@
 URL_WEBPAGE = "https://inspirehep.net/"
 URL_AUTHORS = URL_WEBPAGE + "authors/"
 URL_INSTITUTIONS = URL_WEBPAGE + "institutions/"
-URL_LITERATURE =  URL_WEBPAGE + "literature/"
+URL_LITERATURE = URL_WEBPAGE + "literature/"
 URL_JOBS = URL_WEBPAGE + "jobs/"
 URL_SEMINARS = URL_WEBPAGE + "seminars/"
 URL_CONFERENCES = URL_WEBPAGE + "conferences/"
 
 # affiliations
-AFFILIATIONS_POSITIONS = ["SENIOR", "POSTDOC", "PHD", "UNDERGRADUATE", "MASTER", "MS", "BA", "MSC", "BSC", "MA", "VISITOR"]
+AFFILIATIONS_POSITIONS = [
+        "SENIOR",
+        "POSTDOC",
+        "PHD",
+        "UNDERGRADUATE",
+        "MASTER",
+        "MS", "BA",
+        "MSC",
+        "BSC",
+        "MA",
+        "VISITOR"
+]
