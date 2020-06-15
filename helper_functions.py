9fbe3e50474095b8b334c1eb48753e417cbeb186
javadebadi <javad.ebadi.1990@gmail.com>
2020-06-15 19:16:15 +0430

    Style fix: corrected the style fo the code for helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,5 @@
 # helper functions
-def convert_str_to_int(text = ""):
+def convert_str_to_int(text=""):
     """convert string to number, when the int() function does not work
     Some numbers in the website are like 3,589 which will not be converted
     to 3589 when using int() to the string. This function will help to do
@@ -19,18 +19,18 @@ def convert_str_to_int(text = ""):
         >>> convert_str_to_int(1,000,000)
         1000000
     """
-    l = list(reversed(text.split(",")))
+    tokens = list(reversed(text.split(",")))
     number = 0
-    if len(l) == 0:
+    if len(tokens) == 0:
         return 0
-    for i in range(len(l)):
-        number += (10**(3*i))*int(l[i]) # the numbers were splitted to 3 digits
+    for i in range(len(tokens)):
+        number += (10**(3*i))*int(tokens[i])  # because of 3 digit split
     return number
 
 
 def merge_years_for_two_list(l1, l2):
-    """a private method to merge two list of list where the last element of first list is equal to
-    first element of the second list
+    """a function to merge two list of list where the last element of
+    first list is equal to first element of the second list
 
     Args:
         l1 (list): first list
@@ -40,8 +40,10 @@ def merge_years_for_two_list(l1, l2):
         l (list): merged list of l1 and l2
 
     Example:
-            >>> merge_years_for_two_list(['2013', '2014', '2015'],['2015', '2016', '2019'])
-            ['2013', '2014', '2015', '2016', '2019']
+            >>> merge_years_for_two_list(
+                    ['2013', '2014', '2015'],['2015', '2016', '2019']
+                    )
+            ['2013', '2014', '2015', '2016']
     """
     if len(l1) == 0:
         return l2
@@ -49,10 +51,11 @@ def merge_years_for_two_list(l1, l2):
         return l1
 
     if l1[-1] == l2[0]:
-        l = l1 + l2[1:]
+        result = l1 + l2[1:]
     else:
-        l = l1 + [''] +  l2
-    return l
+        result = l1 + [''] + l2
+    return result
+
 
 def merge_years_for_lists_of_list(list_of_lists):
     """merge list of lists when last element of each list matches with the
@@ -65,21 +68,25 @@ def merge_years_for_lists_of_list(list_of_lists):
         l (list): a merged list of elements in lists of the list
 
     Example:
-        >>> merge_years_for_lists_of_list([["2010", "2012"], ["2012", "2014"], ["2014", "2020"]])
+        >>> merge_years_for_lists_of_list(
+                [["2010", "2012"], ["2012", "2014"], ["2014", "2020"]]
+                )
         ["2010", "2012", "2014", "2020"]
     """
     if len(list_of_lists) < 2:
         return list_of_lists[0]
-    l = []
+    result = []
     for i in range(len(list_of_lists)):
-        l = merge_years_for_two_list(l, list_of_lists[i])
-    return l
+        result = merge_years_for_two_list(result, list_of_lists[i])
+    return result
+
 
 def separate_affiliations_from_pos(text):
     """A function to find affiliation position from a special type of string
 
     Args:
-        text (str): a text which has forms of POSTION,AFFILIATION or AFFILIATION
+        text (str): a text which has forms of
+        POSTION,AFFILIATION or AFFILIATION
 
     Returns:
         pos (str)
