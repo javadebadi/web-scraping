b6492138fd0cd3a5f298e40b494fcd46af64517e
javadebadi <javad.ebadi.1990@gmail.com>
2020-07-16 13:09:57 -0700

    Minor updates

--- tools.py
+++ tools.py
@@ -1,14 +1,16 @@
 # import external packages
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 # import internal packages
 from global_vars import *
 from helper_functions import *
 from InspirehepURL import *
-from Author import *
-from Institutions import *
 from database import *
-
 
 URL = InspirehepURL()
 BROWSER = webdriver.Chrome(PATH_TO_DRIVER)
+DB = DatabaseAccessor()
